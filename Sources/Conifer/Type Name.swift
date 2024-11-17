2474a903ed8c502a088085172ca91c582168e436
Constantino Tsarouhas <c@tsarouhas.eu>
2024-11-17 17:08:03 +0100

    Revert "Makes TypeName.pattern nonisolated since Regex isn’t Sendable."
    This reverts commit f87be99400b2b89bcfbb524b7def4d6228051319.

--- Sources/Conifer/Type Name.swift
+++ Sources/Conifer/Type Name.swift
@@ -12,7 +12,7 @@ public protocol TypeName : Sendable, ExpressibleByStringInterpolation where Stri
 	var source: TypeNameSource { get }
 	
 	/// The pattern that names of this type conform to.
-	static nonisolated var pattern: Regex<Substring> { get }
+	static var pattern: Regex<Substring> { get }
 	
 }
 
