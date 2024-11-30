cc4f18f78f9c47910c3e13fbfb3548256aadc2b5
Constantino Tsarouhas <c@tsarouhas.eu>
2024-11-30 14:53:30 +0100

    Group now supports any number of components. Empty is an empty Group.
    Signed-off-by: Constantino Tsarouhas <c@tsarouhas.eu>

--- Sources/Conifer/Components/Empty.swift
+++ Sources/Conifer/Components/Empty.swift
@@ -1,28 +1,4 @@
 // Conifer © 2019–2024 Constantino Tsarouhas
 
-/// The empty component; a component that represents a component with no body.
-///
-/// ## Shadow Semantics
-///
-/// An empty component does not appear in a shadow. A shadow never contains an `Empty`.
-public struct Empty : Component {	// TODO: Delete when Group<> doesn't cause a compiler crash.
-	
-	/// Creates an empty component.
-	public init() {}
-	
-	// See protocol.
-	public var body: Never { hasNoBody }
-	
-}
-
-extension Empty : FoundationalComponent {
-	
-	func childLocations(for shadow: some Shadow<Self>) async throws -> [Location] {
-		[]
-	}
-	
-	func child(at location: Location, for shadow: some Shadow<Self>) async throws -> any Component {
-		preconditionFailure("\(shadow) does not contain children")
-	}
-	
-}
+/// An empty container component.
+public typealias Empty = Group<>
--- Sources/Conifer/Components/Group.swift
+++ Sources/Conifer/Components/Group.swift
@@ -1,18 +1,15 @@
 // Conifer © 2019–2024 Constantino Tsarouhas
 
-/// A container component consisting of two components in succession.
-///
-/// Groups can be nested to create groups of arbitrary size, e.g., a `Group<Group<A, B>, C>` containing three components `A`, `B`, and `C` in succession.
+/// A container component consisting of any number components in succession.
 ///
 /// ## Shadow Semantics
 ///
-/// A container component is replaced by its two constituent components in a shadow. A shadow never contains a `Group` but instead a `First` and a `Second` in its place. Since there is an ordering between `First` and `Second`, the structural identities of the first and second component are unique.
-public struct Group<First : Component, Second : Component> : Component {	// TODO: Replace with type parameter pack when compiler issues are sorted out.
+/// A group is replaced by its constituent components in a shadow. A shadow never contains a `Group` but instead its children in its place.
+public struct Group<each Child : Component> : Component {
 	
 	/// Creates a group with given children.
-	public init(_ first: First, _ second: Second) {
-		self.first = first
-		self.second = second
+	public init(_ children: repeat each Child) {
+		self.children = (repeat each children)
 	}
 	
 	/// Creates a group using given closure.
@@ -20,23 +17,8 @@ public struct Group<First : Component, Second : Component> : Component {	// TODO
 		self = contents()
 	}
 	
-	/// The first component.
-	public let first: First
-	
-	/// The second component.
-	public let second: Second
-	
-	/// A type that allows concatenating a third component to groups of this type.
-	///
-	/// This convenience syntax allows writing `Group<A, B>.Con<C>` instead of `Group<Group<A, B>, C>`.
-	public typealias Con<Third : Component> = Group<Self, Third>
-	
-	/// Concatenates a third component to this group.
-	///
-	/// This convenience syntax allows writing `Group(a, b).con(c)` instead of `Group(Group(a, b), c)`.
-	public func con<Third : Component>(_ third: Third) -> Self.Con<Third> {
-		.init(self, third)
-	}
+	/// The children.
+	public let children: (repeat each Child)
 	
 	// See protocol.
 	public var body: Never { hasNoBody }
@@ -46,15 +28,31 @@ public struct Group<First : Component, Second : Component> : Component {	// TODO
 extension Group : FoundationalComponent {
 	
 	func childLocations(for shadow: some Shadow<Self>) async throws -> [Location] {
-		[.child(at: 0), .child(at: 1)]
+		var locations = [Location]()
+		var position = 0
+		for _ in repeat each children {
+			locations.append(.child(at: position))
+			position += 1
+		}
+		return locations
 	}
 	
 	func child(at location: Location, for shadow: some Shadow<Self>) async throws -> any Component {
-		switch location {
-			case .child(at: 0):	return first
-			case .child(at: 1):	return second
-			default:			preconditionFailure("\(location) does not exist on \(shadow)")
+		
+		guard case .position(let desiredPosition)? = location.directions.first, location.directions.count == 1 else {
+			preconditionFailure("\(location) does not refer to a positional child in \(shadow)")
 		}
+		
+		var position = 0
+		for child in repeat each children {
+			if position == desiredPosition {
+				return child
+			}
+			position += 1
+		}
+		
+		preconditionFailure("\(shadow) does not have a child at position \(desiredPosition)")
+		
 	}
 	
 }
